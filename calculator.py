68ee8afd279267267985d9b3cab2dba5ec75b02d
trader87 <ironbone98@gmail.com>
2025-06-15 07:21:43 +0700

    add calculator gui

---
+++ calculator.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import ttk, messagebox
+
+GUI = Tk()
+GUI.title('Basic Git & GitHub by Uncle Engineer')
+GUI.geometry('500x700')
+
+L = Label(GUI, text='Traderr87')
+L.pack()
+
+GUI.mainloop()
